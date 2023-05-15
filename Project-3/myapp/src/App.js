4a55b0185f3fe9515765a7a0a0f956fdf4e0743b
Chetan Gupta <chetangupta2708@gmail.com>
2023-05-15 23:15:41 +0530

    React_Cart

---
+++ Project-3/myapp/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import Cart from "./components/Cart";
+
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+export default App;
