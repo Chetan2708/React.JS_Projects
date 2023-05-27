d1c3da6178b9a06b63df47dfd93e73d3cac2410a
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-05-27 23:44:49 +0530

    Crypto Coin TraderHQ

--- Project_2/src/components/Home.jsx
+++ Project_2/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import btc from "../Assets/btc1.jpg";
-import{motion} from "framer-motion"
+import { motion } from "framer-motion";
+
 const Home = () => {
   return (
     <Box bgColor={"navyblue"} w={"full"} h={"85vh"}>
-
-<motion.div
+      {/* This is a motion div */}
+      <motion.div
         style={{
           height: "85vh",
         }}
@@ -19,22 +20,18 @@ const Home = () => {
           repeatType: "reverse",
         }}
       >
-        <Image
-     w={"full"}
-      h={"full"} 
-      objectFit={"cover"}
-       src={btc}
-        />
+        <Image w={"full"} h={"full"} objectFit={"cover"} src={btc} />
 
-      <Text
-        fontSize={"6xl"}
-        textAlign={"center"}
-        fontWeight={"thin"}
-        color={"blackAlpha.800"}
-        mt={"-20"}
-        > CoinTraderHQ
-      </Text>
-        </motion.div>
+        <Text
+          fontSize={"6xl"}
+          textAlign={"center"}
+          fontWeight={"thin"}
+          color={"blackAlpha.800"}
+          mt={"-20"}
+        >
+          CoinTraderHQ
+        </Text>
+      </motion.div>
     </Box>
   );
 };
