808fc2c4184cbb6e288f162a879df8267191bda0
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-08-06 23:42:00 +0530

    .

--- Gym App/vite-project/src/components/ExerciseCard.jsx
+++ Gym App/vite-project/src/components/ExerciseCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import {Box, Stack ,Typography,Button}from '@mui/material'
 const ExerciseCard = ({i}) => (
-    <Link className="exercise-card" to={`/exercise/${i.id}`} >
+    <Link className="exercise-card"  to={`/exercise/${i.id}`}   >
         <img src={i.gifUrl} alt={i.name} loading='lazy' />
         <Stack direction="row">
       <Button sx={{ ml: '21px', color: '#fff', background: 'pink', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
@@ -12,11 +12,11 @@ const ExerciseCard = ({i}) => (
         {i.target}
       </Button>
     </Stack>
-          <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
+          <Typography  ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
       {i.name}
     </Typography>
     </Link>
 
 )
 
-export default ExerciseCard
+export default ExerciseCard //Export the exercise card 
