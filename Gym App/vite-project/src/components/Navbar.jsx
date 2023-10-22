c17d9beeecc89ee6564547b7db8659f9c03e3464
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-10-22 23:57:28 +0530

    .

--- Gym App/vite-project/src/components/Navbar.jsx
+++ Gym App/vite-project/src/components/Navbar.jsx
@@ -29,6 +29,7 @@ const Navbar = () => {
         fontFamily="Alegreya"
         fontSize="24px"
         alignItems="flex-end"
+        
         divider={<Divider orientation="vertical" flexItem/>}
       >
         <Link
@@ -36,6 +37,7 @@ const Navbar = () => {
           style={{
             textDecoration: "none",
             color: "#3A1212",
+            
             borderBottom: "3px solid #FF2625",
           }}
         >
