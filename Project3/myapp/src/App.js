7eed0dfd6415cc9985688779aba8caa4541afecb
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2024-01-05 22:53:19 +0530

    Update App.js

--- Project3/myapp/src/App.js
+++ Project3/myapp/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
       <Toaster />
