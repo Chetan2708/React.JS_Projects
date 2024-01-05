dc37ef13cdd4212f65689370004a55b4c62a5f9e
Chetan Gupta <chetangupta2708@gmail.com>
2024-01-05 22:55:01 +0530

    Initial

--- Project3/myapp/src/components/Header.jsx
+++ Project3/myapp/src/components/Header.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {FiShoppingBag} from "react-icons/fi"
-import { useSelector} from "react-redux";
-import { FaAmazon } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
+import { FaAmazon } from 'react-icons/fa';
+
 const Header = () => {
   const { cartitems } = useSelector((state) => state.Cart);
-  return (
-      <nav>
-        <h2>
-        <Link to={"/"}><FaAmazon/></Link>
-        </h2>
-        <div>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/cart"}>
-          <FiShoppingBag/><p>{cartitems.length}</p>
-         </Link> 
-        </div>
-      </nav>
-  )
-}
 
-export default Header
+
+  return (
+    <nav>
+      <h2>
+        <Link to={'/'}>
+          <FaAmazon />
+        </Link>
+      </h2>
+      <div>
+        <Link to={'/home'}>Home</Link>
+        
+        <Link to={'/cart'}>
+          <FiShoppingBag />
+          <p>{cartitems.length}</p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
